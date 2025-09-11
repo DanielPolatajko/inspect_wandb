d17fe28e3d9de02852c19ac27249f827854276d9
DanielPolatajko <danielpolatajko@gmail.com>
2025-09-11 09:55:27 +0100

    add environment validations

--- inspect_wandb/config/settings.py
+++ inspect_wandb/config/settings.py
@@ -1,8 +1,16 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
 from typing import Any
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic_settings.sources import PydanticBaseSettingsSource, PyprojectTomlConfigSettingsSource
 from inspect_wandb.config.wandb_settings_source import WandBSettingsSource
+import os
+
+class EnvironmentValidations(BaseModel):
+    """
+    A set of environment variables which should be validated before enabling the integration.
+    """
+    wandb_base_url: str | None = Field(default=None, description="The base URL of the wandb instance")
+    wandb_api_key: str | None = Field(default=None, description="The API key for the wandb instance")
 
 class ModelsSettings(BaseSettings):
     """
@@ -27,7 +35,20 @@ class ModelsSettings(BaseSettings):
     add_metadata_to_config: bool = Field(default=True, description="Whether to add eval metadata to wandb.config")
 
     tags: list[str] | None = Field(default=None, description="Tags to add to the models run")
+    environment_validations: EnvironmentValidations | None = Field(default=None, description="Environment variables to validate before enabling")
 
+    @field_validator("environment_validations", mode="after")
+    @classmethod
+    def validate_environment_variables(cls, v: EnvironmentValidations | None) -> EnvironmentValidations | None:
+        if v is not None:
+            if v.wandb_base_url is not None and (env_wandb_base_url := os.getenv("WANDB_BASE_URL")) != v.wandb_base_url:
+                cls.enabled = False
+                raise ValueError(f"WANDB_BASE_URL does not match the value in the environment. Validation URL: {v.wandb_base_url}, Environment URL: {env_wandb_base_url}")
+            if v.wandb_api_key is not None and (env_wandb_api_key := os.getenv("WANDB_API_KEY")) != v.wandb_api_key:
+                cls.enabled = False
+                raise ValueError(f"WANDB_API_KEY does not match the value in the environment. Validation Key: {v.wandb_api_key}, Environment Key: {env_wandb_api_key}")
+        return v
+    
     @classmethod
     def settings_customise_sources(
         cls,
